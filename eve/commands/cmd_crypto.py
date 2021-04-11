b098462d2a911b0cd2a8e3aa799140178f10c366
IVIURRAY <haydn.murray1@yahoo.co.uk>
2021-04-11 16:04:18 +0100

    adding crypto gecko fallback

--- eve/commands/cmd_crypto.py
+++ eve/commands/cmd_crypto.py
@@ -2,8 +2,7 @@ import click
 import locale
 from eve.service import svc_crypto
 from eve.config import DEFAULT_COIN, CURRENCY
-from eve.utilities import utils as f
-
+from eve.utilities import utils
 
 class Context:
     def __init__(self, coin, currency):
@@ -27,9 +26,11 @@ def price(ctx):
     """Price in another currency"""
     locale.setlocale(locale.LC_ALL, "")
     result = ctx.obj.crypto.price(coin=ctx.obj.coin, currency=ctx.obj.currency)
-    price_value, price_currency = result["price"].split(" ")
-    click.echo(f' {result["symbol"]} ({result["name"].upper()}) PRICE '.center(45, "="))
-    click.echo(f'\U0001F4B1 Price:         {f.format_float_from_str(price_value, 4)} {price_currency}')
-    click.echo(f'\U0001F4B0 Market cap:    {f.format_float_from_str(result["market_cap"], 4)} {price_currency}'),
-    click.echo(f'\U0001F3E6 TTL supply:    {f.format_int_from_str(result["total_supply"])}')
-    click.echo(f'\U0001F4C8 24H change:    {f.format_float_from_str(result["percent_change_24h"], 1)} %')
+    price_value = result["value"]
+    price_currency = result['currency']
+    total_supply = utils.format_int_from_str(result["total_supply"]) if result.get('total_supply') else 'Not Available'
+    click.echo(f' {result.get("symbol")} ({result["name"].upper()}) PRICE ({result["source"]}) '.center(45, "="))
+    click.echo(f'\U0001F4B1 Price:         {utils.format_float_from_str(price_value, 4)} {price_currency}')
+    click.echo(f'\U0001F4B0 Market cap:    {utils.format_float_from_str(result["market_cap"], 4)} {price_currency}'),
+    click.echo(f'\U0001F3E6 TTL supply:    {total_supply}')
+    click.echo(f'\U0001F4C8 24H change:    {utils.format_float_from_str(result["percent_change_24h"], 1)} %')
--- eve/service/svc_crypto.py
+++ eve/service/svc_crypto.py
@@ -1,23 +1,60 @@
-from coinmarketcapapi import CoinMarketCapAPI
+from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
+from pycoingecko import CoinGeckoAPI
 from eve.config import COIN_MARKET_CAP_API_KEY
 
 
 class Crypto:
-
     def __init__(self, api_key=None):
-        self.manager = CoinMarketCapAPI(api_key or COIN_MARKET_CAP_API_KEY)
+        self.coin_market_cap = CoinMarketCap(api_key=api_key)
+        self.gecko = Gecko()
+
+    def price(self, coin, currency):
+        try:
+            return self.coin_market_cap.price(coin, currency)
+        except CoinMarketCapAPIError:
+            return self.gecko.price(coin, currency)
+
+
+class CoinMarketCap:
+    def __init__(self, api_key=None):
+        self.api = CoinMarketCapAPI(api_key or COIN_MARKET_CAP_API_KEY)
 
     @staticmethod
     def filter_essential_data(data, coin, currency):
         return {
+            'source': 'Coin Market Cap',
             'symbol': data[coin]["symbol"],
             'name': data[coin]["name"],
             'total_supply': data[coin]["total_supply"],
-            'price': f"{data[coin]['quote'][currency]['price']} {currency}",
+            'value': f"{data[coin]['quote'][currency]['price']}",
+            'currency':  currency,
             'percent_change_24h': data[coin]["quote"][currency]["percent_change_24h"],
             'market_cap': data[coin]["quote"][currency]["market_cap"]
         }
 
     def price(self, coin, currency):
-        result = self.manager.cryptocurrency_quotes_latest(symbol=coin, convert=currency)
+        result = self.api.cryptocurrency_quotes_latest(symbol=coin, convert=currency)
         return self.filter_essential_data(result.data, coin=coin, currency=currency)
+
+
+class Gecko:
+    def __init__(self):
+        self.api = CoinGeckoAPI()
+
+    @staticmethod
+    def filter_essential_data(data, coin, currency):
+        currency = currency.lower()
+        return {
+            'source': 'Gecko',
+            'name': coin,
+            'currency': currency,
+            'value': float(data[currency]),
+            'percent_change_24h': data[f"{currency}_24h_change"],
+            'market_cap': data[f"{currency}_market_cap"]
+        }
+
+    def price(self, coin, currency):
+        result = self.api.get_price(
+            ids=coin, vs_currencies=currency, include_market_cap='true', include_24hr_change='true'
+        )
+        return self.filter_essential_data(result[coin.lower()], coin=coin, currency=currency)
