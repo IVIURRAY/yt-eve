e347224fa4a3663f4e99cf669fc2c321b580a638
FrankBlack78 <44316016+FrankBlack78@users.noreply.github.com>
2021-05-02 19:59:14 +0200

    Updated svc_note.py
    Made some minor changes to svc_note.py (e. g. colorcoding for reached due-dates and highlights for outputs).

--- eve/service/svc_note.py
+++ eve/service/svc_note.py
@@ -9,15 +9,21 @@ import pathlib
 
 class Note:
     def __init__(self):
+        # Console API from rich module
+        self.console = Console()
+        # Highlight color 1 for outputs ==> Affected objects (e. g. files, properties etc.)
+        self.color = 'bold dodger_blue1'
+        # Highlight color 2 for outputs ==> Warnings (e. g. reached due dates etc.)
+        self.color2 = 'deep_pink2'
         # Path to note-file
-        self.note_file = os.path.join(pathlib.Path.home(), 'test2.json')  # rename for production and make it a hidden file
+        self.note_file = os.path.join(pathlib.Path.home(), '.eve_note.json')
         # Check if note-file exists and if not create and populate it with sample data
         if not os.path.isfile(self.note_file):
             self.touch = '''
             {
             "1": {"CreationDate": "2021-04-24",
                 "EditDate": null,
-                "DueDate": "2021-04-29",
+                "DueDate": "2030-10-29",
                 "Priority": null,
                 "NoteText": "This is sample entry 1."},
             "2": {"CreationDate": "2021-04-24",
@@ -35,14 +41,14 @@ class Note:
             self.touch = json.loads(self.touch)
             with open(self.note_file, 'w', encoding='utf-8') as f:
                 json.dump(self.touch, f, indent=2, ensure_ascii=False)
-            print(f'{self.note_file} created...')
+            self.console.log(f'[{self.color}]{self.note_file}[/] created...')
         # Load note-file into note-data
         with open(self.note_file, encoding='utf-8') as f:
             self.note_data = json.load(f)
-            print(f'{self.note_file} loaded into class-object...')
+            self.console.log(f'[{self.color}]{self.note_file}[/] loaded into class-object...')
 
     def output(self):
-        # Create copy of note-data for output (because of the transformation-process)
+        # Create copy of note-data for output (because of the transformation-process necessary for rich-table)
         output_data = self.note_data
         # Transform None-values of output-data to str('None') and the other values to str()
         for key in output_data:
@@ -54,19 +60,18 @@ class Note:
         # Order output-data by 'Priority' and 'DueDate'
         output_data = dict(sorted(output_data.items(), key=lambda x: x[1]['Priority']))
         output_data = dict(sorted(output_data.items(), key=lambda x: x[1]['DueDate']))
-        # Create table for output
-        console = Console()
+        # Create rich-table for output
         table = Table(show_header=True,
                       box=box.ASCII,
                       # show_lines=True,
-                      header_style='bold blue')
+                      header_style=self.color)
         table.add_column('Key', style='dim')
         table.add_column('CreationDate', style='dim')
         table.add_column('EditDate', style='dim')
         table.add_column('DueDate', style='dim')
         table.add_column('Priority', style='dim')
         table.add_column('NoteText')
-        # Populate table with output-data
+        # Populate rich-table with output-data
         for key in output_data:
             row = list()
             row.append(key)
@@ -75,9 +80,14 @@ class Note:
             row.append(output_data[key]['DueDate'])
             row.append(output_data[key]['Priority'])
             row.append(output_data[key]['NoteText'])
-            table.add_row(str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]))
-        # Print table to console
-        console.print(table)
+            # Check if DueDate is <= current date and highlight DueDate if it is
+            if row[3] <= str(date.today()):
+                table.add_row(str(row[0]), str(row[1]), str(row[2]), str(f'[{self.color2}]'+row[3]+'[/]'),
+                              str(row[4]), str(row[5]))
+            else:
+                table.add_row(str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]))
+        # Print rich-table to console
+        self.console.print(table)
 
     def add(self, note_text=None, due_date=None, priority=None):
         # Compose new note
@@ -88,34 +98,34 @@ class Note:
         new_note["DueDate"] = due_date
         new_note["Priority"] = priority
         new_note["NoteText"] = note_text
-        # Append new_note to note_data
+        # Append new note to note-data
         self.note_data[key] = new_note
-        print(f'New note with key {key} added...')
-        # Save note_data to note_file
+        self.console.log(f'New note with key [{self.color}]{key}[/] added...')
+        # Save note-data to note-file
         self.save()
 
     def delete(self, key=None):
         if key is None:
-            print(f'No key given...')
+            self.console.log(f'No key given...')
         else:
-            # Delete key value from note_data for given key
+            # Delete key value from note-data for given key
             try:
                 del self.note_data[key]
-                print(f'Key {key} deleted...')
-                # Assign new keys to note_data
+                self.console.log(f'Key [{self.color}]{key}[/] deleted...')
+                # Assign new keys to note-data
                 new_keys = list()
                 for i in range(len(self.note_data)):
                     new_keys.append(str(i + 1))
                 self.note_data = dict(zip(new_keys, list(self.note_data.values())))
-                print(f'New enumeration for note-data applied...')
+                self.console.log(f'New enumeration for note-data applied...')
                 # Save note-data to note-file
                 self.save()
             except KeyError:
-                print(f'Key {key} not existing...')
+                self.console.log(f'Key [{self.color}]{key}[/] not existing...')
 
     def edit(self, key=None, **kwargs):
         if key is None:
-            print(f'No key given...')
+            self.console.log(f'No key given...')
         else:
             # Edit values for given key
             edit_counter = 0
@@ -123,23 +133,23 @@ class Note:
                 self.note_data[key]['DueDate'] = kwargs['DueDate']
                 edit_counter += 1
                 prop = kwargs['DueDate']
-                print(f'DueDate of key {key} changed to \'{prop}\'...')
+                self.console.log(f'DueDate of key [{self.color}]{key}[/] changed to [{self.color}]\'{prop}\'[/]...')
             except KeyError:
-                print(f'DueDate of key {key} not changed...')
+                self.console.log(f'DueDate of key [{self.color}]{key}[/] not changed...')
             try:
                 self.note_data[key]['Priority'] = kwargs['Priority']
                 edit_counter += 1
                 prop = kwargs['Priority']
-                print(f'Priority of key {key} changed to \'{prop}\'...')
+                self.console.log(f'Priority of key [{self.color}]{key}[/] changed to [{self.color}]\'{prop}\'[/]...')
             except KeyError:
-                print(f'Priority of key {key} not changed...')
+                self.console.log(f'Priority of key [{self.color}]{key}[/] not changed...')
             try:
                 self.note_data[key]['NoteText'] = kwargs['NoteText']
                 edit_counter += 1
                 prop = kwargs['NoteText']
-                print(f'NoteText of key {key} changed to \'{prop}\'...')
+                self.console.log(f'NoteText of key [{self.color}]{key}[/] changed to [{self.color}]\'{prop}\'[/]...')
             except KeyError:
-                print(f'NoteText of key {key} not changed...')
+                self.console.log(f'NoteText of key [{self.color}]{key}[/] not changed...')
             # Set EditDate and save note-data to note-file if an edit happened
             if edit_counter > 0:
                 self.note_data[key]['EditDate'] = str(date.today())
@@ -148,14 +158,4 @@ class Note:
     def save(self):
         with open(self.note_file, 'w', encoding='utf-8') as f:
             json.dump(self.note_data, f, indent=2, ensure_ascii=False)
-        print(f'{self.note_file} saved...')
-
-
-# a = Note()
-# a.output()
-# a.add('This is a manual entry 1: Test äöüÄÖÜß')
-# a.add('This is a manual entry 2', '2021-05-30')
-# a.add('This is a manual entry 3', '2021-04-30', 2)
-# a.delete('1')
-# a.edit(key='3', DueDate='2021-04-28', Priority=1, NoteText='Entry edited.')
-# a.output()
+        self.console.log(f'[{self.color}]{self.note_file}[/] saved...')
