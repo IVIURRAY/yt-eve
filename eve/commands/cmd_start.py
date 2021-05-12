e3ddcfa9107e552c5194e282026bbeec92977f47
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-05-12 11:01:06 +0530

    Removed -y flag

--- eve/commands/cmd_start.py
+++ eve/commands/cmd_start.py
@@ -7,27 +7,10 @@ from typing import Dict, Union
 import click
 
 from eve.service.svc_start import Start, get_langs_and_licenses
+from eve.config import AUTHOR_EMAIL, AUTHOR_NAME
 
 START_META = os.path.join(
     Path(os.path.dirname(__file__)).parent, "meta", "start")
-
-with open(os.path.join(START_META, "default.json"), "r") as f:
-    data = json.load(f)
-
-
-def get_default_values(collect: bool = False) -> Union[None, Dict[str, Dict[str, str]]]:
-    # if collect is true then collect the info and store in default else return the default values
-    if collect:
-        req_fields = ["author", "author_email"]
-        for r in req_fields:
-            val = click.prompt(f"{r}")
-            data["data"][r] = val
-
-        with open(os.path.join(START_META, "default.json"), "w") as f:
-            json.dump(data, f)
-
-    else:
-        return data
 
 
 @click.command()
@@ -37,9 +20,8 @@ def get_default_values(collect: bool = False) -> Union[None, Dict[str, Dict[str,
 @click.option("-d", "--directory", type=str, help="The directory to create all the starter files and folders.", default=".", show_default=True)
 @click.option("--env", help="Create a virtual env for Python projects", is_flag=True)
 @click.option("--set-default", help="Add default values", is_flag=True)
-@click.option("-y", type=bool, is_flag=True, help="Use default values while making the starter files.")
 @click.option("--no-git", type=bool, is_flag=True, default=False, help="Do not initialize a git repo", show_default=False)
-def cli(language: str, name: str, directory: str, license: str, y: bool, env: bool, set_default: bool, no_git: bool) -> None:
+def cli(language: str, name: str, directory: str, license: str, env: bool, set_default: bool, no_git: bool) -> None:
     """Start a new project in any language"""
 
     if not Path(directory).is_dir():
@@ -49,13 +31,10 @@ def cli(language: str, name: str, directory: str, license: str, y: bool, env: bo
 
     if not set_default:
         langs_and_lice = get_langs_and_licenses()
-        data = {"data": {"author": "", "author_email": ""}}
-        if y:
-            data = get_default_values()
 
         if name and language:
             if language in langs_and_lice["langs"]:
-                if license not in langs_and_lice["lice"]:
+                if license.lower() not in langs_and_lice["lice"]:
                     click.echo(click.style(
                         "unrecognized license. Using MIT instead.", fg="yellow"))
                     click.echo(click.style(
@@ -63,8 +42,8 @@ def cli(language: str, name: str, directory: str, license: str, y: bool, env: bo
 
                 start = Start()
                 start.name = name
-                start.author = data["data"]["author"]
-                start.author_email = data["data"]["author_email"]
+                start.author = AUTHOR_NAME
+                start.author_email = AUTHOR_EMAIL
                 start.directory = directory
                 start.license = license
                 start.make_env = env
@@ -82,4 +61,4 @@ def cli(language: str, name: str, directory: str, license: str, y: bool, env: bo
                 "Name and language are required.", fg="red"))
 
     else:
-        get_default_values(collect=True)
+        pass  # : TODO: find a way to set environment values
